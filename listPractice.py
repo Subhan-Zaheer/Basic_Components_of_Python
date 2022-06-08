dd15fa7a7e8b96cfa4e11f7d52758a9390df6567
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-06-08 15:38:20 +0500

    List Practice

---
+++ listPractice.py
@@ -0,0 +1,17 @@
+"""
+ls = []
+print("Enter your Entries : ")
+for i in range(5):
+    item=input("Enter Course Name : ")
+    ls.append(item)
+    """
+ls = ["DAA", "TOA", "DBS", "OS", "QT"]
+print(ls[:0:-1])
+ls.insert(2, "DE")
+print(ls)
+a = 8
+b = 9
+c = 7
+print(a, b, c)
+a, b = b, c
+print(a, b)
