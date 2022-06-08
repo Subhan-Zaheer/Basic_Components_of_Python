0e23488d4d478baa8ffbfb9c688fbf5e966d4a52
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-06-08 15:36:48 +0500

    Some Changes done

--- DIctionaryPractice.py
+++ DIctionaryPractice.py
@@ -4,18 +4,18 @@ dict = {"Immutable": "Immutable is something that can\'t be change.",
         "List": "It is a List of Items and it can be change i.e it is Mutable.",
         "Tuple": "Tuple is same as List but can\' be change i.e it is Immutable.",
         "Dictionary": "It is a Key:Value pair and it\'s Key is Immutable whereas Value can be change."}
-while "true":
-    print("Do you want to play this game again.")
-    print("Enter \'Y\' if Yes otherwise \'N\' : ")
-    anotherInput = input()
-    if anotherInput == 'y' or anotherInput == 'Y':
-        print("Enter a Key from Following : \nImmutable\nMutable\nList\nTuple\nDictionary\n"
-              "and we will Description of that Key.")
-        input1 = input("Enter your choice : ")
-        print(dict[input1])
-
-    else :
-        exit()
+# while "true":
+#     print("Do you want to play this game again.")
+#     print("Enter \'Y\' if Yes otherwise \'N\' : ")
+#     anotherInput = input()
+#     if anotherInput == 'y' or anotherInput == 'Y':
+#         print("Enter a Key from Following : \nImmutable\nMutable\nList\nTuple\nDictionary\n"
+#               "and we will Description of that Key.")
+#         input1 = input("Enter your choice : ")
+#         print(dict[input1])
+#
+#     else:
+#         exit()
 
 
 
