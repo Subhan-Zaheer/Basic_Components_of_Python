5d4b4a9b0324d165bb42eb62e0e2e11a396c9ea1
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-06-08 15:40:18 +0500

    Some Basic String Function

---
+++ stringPractice.py
@@ -0,0 +1,7 @@
+str1 = "My name is {myName} Zaheer."
+print(str1[17:10:-1])
+print(str1.endswith("eer."))
+print(str1.count("e"))
+print(str1.find("name"))
+print(str1.replace("name is ", "self "))
+print(str1.format(myName="Subhan"))
