2c8f08c3d2cc231d84d470cb353e51d323136c2a
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-06-09 15:53:08 +0500

    Short hand If else statement

---
+++ Short_hand_IfElse.py
@@ -0,0 +1,5 @@
+a = int(input("Enter a number : "))
+b = int(input("Enter a number : "))
+# if a > b: print("A B is bigger hai ")
+
+print("A is bigger than B") if a > b else print("B is bigger than A")
