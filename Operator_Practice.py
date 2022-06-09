a65fa6ff438685fabf44c87db8229bebc5796b9c
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-06-09 15:40:38 +0500

    Some basic operators in Pyton

---
+++ Operator_Practice.py
@@ -0,0 +1,43 @@
+#Operators in Python
+# Arithmetic Operators
+# Assignment Operator
+# Comparison Operators
+# Logical Operators
+# Identity Operators
+# Membership Operators
+# Bitwise Operators
+
+# Arithmetic Operators
+# print("5 + 6 is : ", 5 + 6)
+# print("5 - 6 is : ", 5 - 6)
+# print("5 ** 6 is : ", 5 ** 6)
+# print("5 / 6 is : ", 5 / 6)
+# print("25 // 6 is : ", 25 // 6)
+
+# Assignment Operator
+# x = 5
+# x += 6
+# print("Value of x is : ", x)
+
+# Identity Operators
+# (is) and (is not)
+#
+# a = True
+# b = False
+# print(a is not b)
+
+# Membership Operators
+# (in) and (not in)
+# ls = [1, 2, 3, 4]
+# print(1 not in ls)
+
+# Bitwise Operators
+# Bitwise | operator and & operator
+# This Operator will calculate the value of two operands bit wise
+# 00
+# 11 if we are doing | operator
+# --
+# 11
+# print(0 & 3)
+# print(0 | 3)
+
