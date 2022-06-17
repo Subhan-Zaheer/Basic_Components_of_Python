dad5bd1c2e4e3e89348cd37f84e564c53ded1f15
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-06-17 18:59:23 +0500

    Python modules with example of RANDOM and OPERATOR module

---
+++ modules.py
@@ -0,0 +1,19 @@
+import random
+random_number = random.randint(0,8)
+print(random_number)
+
+rand = random.random() * 100
+print(rand)
+ls = ["Subhan", "Hassaan", "Ahmad", "Hamza"]
+choice = random.choice(ls)
+print (choice)
+
+import operator
+print(operator.add(5, 6))
+
+num1 = int(input("Enter a number1 : "))
+num2 = int(input("Enter a number2 : "))
+if operator.eq(num1, num2):
+    print("Both numbers are equal.")
+else:
+    print("Numbers are not equal.")
