6ab7b2653b6dd8482e28f873e0753b6e927c6196
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-06-23 17:00:07 +0500

    Simple Class example

---
+++ oopAndClasses.py
@@ -0,0 +1,19 @@
+# Class - Template
+# Object - Instance of Class
+
+#DRY - Do not repeat Yourself
+
+#First Class
+
+class Student:
+    pass
+
+Subhan = Student()
+Ahmad = Student()
+
+Subhan.name = "Subhan"
+Subhan.sec = "B"
+Subhan.degree = "BSCS"
+Subhan.course = ["DB", "OS", "DAA", "TOA", "DE", "QT"]
+
+print(Subhan.course)
