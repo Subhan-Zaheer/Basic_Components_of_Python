66b0545384ab04b45f6f6cd3f1f53ca7e6ba1e17
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-06-23 22:31:31 +0500

    function added in class

--- Instance_and_object.py
+++ Instance_and_object.py
@@ -1,6 +1,7 @@
 class Employee:
     no_of_workingHours = 9
-    pass
+    def printFunc(self):
+        return f"Name of Employee is {self.name}. Salary of Employee is {self.sal}. Working hours of Employee is {self.no_of_workingHours}"
 
 subhan = Employee()
 ahmad = Employee()
@@ -8,6 +9,7 @@ subhan.name = "Subhan Zaheer"
 subhan.sal = 10000
 ahmad.name = "Ahmad Iqbal"
 ahmad.sal = 10000
-print(subhan.__dict__)
-print(Employee.__dict__)
-print(ahmad.__dict__)
+# print(subhan.__dict__)
+# print(Employee.__dict__)
+# print(ahmad.__dict__)
+print(subhan.printFunc())
