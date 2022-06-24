85a6f321e9278479cd2de3bec5392a836ab96c8e
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-06-24 16:01:29 +0500

    Alternate Constructors done

--- AlternateConstructors.py
+++ AlternateConstructors.py
@@ -4,12 +4,17 @@ class Employee:
         self.name = aname
         self.salary = asalary
         print(f"My name is {self.name} and my salary is {self.salary}")
-
+    #
     def printFunc(self):
         return f"Name of Employee is {self.name}. Salary of Employee is {self.salary}. Working hours of Employee is {self.no_of_workingHours}"
     @classmethod #This is class method
     def change_hours(cls, newhours):
         cls.no_of_workingHours = newhours
+    @classmethod
+    def from_Str(cls, new_string):
+        # params = new_string.split("-")
+        # return cls(params[0], params[1])
+        return cls(*new_string.split("-"))
 
 subhan = Employee("Subhan", 100000)
 ahmad = Employee("Ahmad", 100000)
@@ -21,9 +26,11 @@ ahmad = Employee("Ahmad", 100000)
 # print(Employee.__dict__)
 # print(ahmad.__dict__)
 # print(subhan.printFunc())
-print(subhan.no_of_workingHours)
-Employee.no_of_workingHours = 11
-print(Employee.no_of_workingHours)
-print(subhan.no_of_workingHours)
-subhan.change_hours(7)
-print(subhan.no_of_workingHours)
+# print(subhan.no_of_workingHours)
+# Employee.no_of_workingHours = 11
+# print(Employee.no_of_workingHours)
+# print(subhan.no_of_workingHours)
+# subhan.change_hours(7)
+# print(subhan.no_of_workingHours)
+Hassaan = Employee.from_Str("Hassaan Ahmad-90000")
+print(Hassaan.name)
