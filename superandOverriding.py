a42d345243b217e3f51294011796bf0afce87124
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-06-25 16:31:32 +0500

    Some commenting done

--- superandOverriding.py
+++ superandOverriding.py
@@ -1,16 +1,16 @@
 class A:
-    classvar1 = "I am a class variable in class A."
+    classvar1 = "I am a class variable in class A." # Class Variable
     def __init__(self, var):
-        self.var1 = "I am a var 1 in class A."
+        self.var1 = "I am a var 1 in class A." # Instance variable.
         self.classvar1 = "Instance variable in class A constructor."
         self.special = "Special Variable"
         print(f"Variable is {var}")
 
 class B(A):
-    classvar2 = "I am a class variable in class B."
+    classvar2 = "I am a class variable in class B." # Class Variable
     def __init__(self):
-        self.var1 = "I am a var 1 in class B."
-        super().__init__("Var B")
+        self.var1 = "I am a var 1 in class B." #Instance variable.
+        super().__init__("Var B") # Parent class constructor with parameter.
         self.classvar1 = "Instance variable in class B constructor."
 
 a = A("Var")
