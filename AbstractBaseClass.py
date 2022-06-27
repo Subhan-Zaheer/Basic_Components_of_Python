2526f879f4543e2199a4fdfc684e8f8fc3170c7d
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-06-27 23:52:18 +0500

    __str__ Dunder Function added as Abstract Method

--- AbstractBaseClass.py
+++ AbstractBaseClass.py
@@ -6,6 +6,8 @@ class Shape(metaclass=ABCMeta):
     @abstractmethod
     def printArea(self):
         return 0
+    def __str__(self):
+        return 0
 
 class Rectangle(Shape):
     type = "Rectangle"
@@ -15,6 +17,8 @@ class Rectangle(Shape):
         self. width = 7
     def printArea(self):
         return self.length * self.width
+    def __str__(self):
+        return f"The type of this shape is {self.type} and its sides are {self.sides}"
 
 class Square(Shape):
     type = "Square"
@@ -23,8 +27,12 @@ class Square(Shape):
         self.one_side = 6
     def printArea(self):
         return self.one_side * self.one_side
+    def __str__(self):
+        return f"The type of this shape is {self.type} and its sides are {self.sides}"
+
 
 rect = Rectangle()
 sq = Square()
 print(f"So the area of Rectangle, where its length is 6cm and width is 7cm, is : " + str(rect.printArea()))
 print(f"So the area of Square, where its one side is 6cm, is : " + str(sq.printArea()))
+print(sq)
