254029cd09e784c294a5071cd42c21b271cced10
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-07-15 23:41:29 +0500

    Some changes done

--- reqModule.py
+++ reqModule.py
@@ -8,12 +8,17 @@ import requests
 # r = requests.get("https://www.youtube.com")
 # print(r.text)
 # print(r.status_code)
-url = "https://www.facebook.com"
+url = "https://www.facebook.com/cookies"
+cookies = {"key1" : "Value1"}
 data = {
-    "email" : "subhanzaheer2003@yahoo.com",
+    "email" : "subhanzaheer88@yahoo.com",
     "password" : "Muhammad.63"
 }
-r2 = requests.post(url= url, params= data)
+r2 = requests.get(url= url, cookies= cookies)
 print(r2)
 print(r2.status_code)
 print(r2.url)
+# print(r2.text)
+print(r2.cookies.values())
+# print(r2.headers)
+# print(r2.content)
